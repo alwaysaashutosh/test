2985ed6b755a87851d19324800f5fdc23b5303ed
ashutosh-the-beast <ashutosh@accuknox.com>
2022-11-02 09:55:39 +0530

    second commit

---
+++ 1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-co-op/gocron"
+)
+
+func main() {
+	s := gocron.NewScheduler(time.UTC)
+	t := 1
+
+	_, _ = s.Every(t).Second().SingletonMode().Do(numbers)
+
+	_, _ = s.Every(t).Second().SingletonMode().Do(alphabets)
+
+	s.StartBlocking()
+}
+
+func numbers() {
+
+	fmt.Println("Hello Numbers")
+	// for i := 1; i <= 10000; i++ {
+	// 	// time.Sleep(2 * time.Millisecond)
+	// 	fmt.Printf("%d ", i)
+	// }
+
+}
+
+func alphabets() {
+	fmt.Println("Hello Alphabets")
+	// for i := 'a'; i <= 'e'; i++ {
+	// 	// time.Sleep(3 * time.Millisecond)
+	// 	fmt.Printf("%c ", i)
+	// }
+}
